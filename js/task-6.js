bab2225dd1080a70e8df3d34fc31a8c2909cf5c0
Sashiy1 <aleksander.paluch@wp.pl>
2024-01-08 13:46:22 +0100

    55

--- js/task-6.js
+++ js/task-6.js
@@ -4,21 +4,5 @@ function getRandomHexColor() {
     .padStart(6, '0')}`;
 }
 
-const createEl = document.querySelector('[data-create]');
-const boxesContainer = document.querySelector('#boxes');
-
-let size = 30; // Początkowy rozmiar kwadratu
-
-const createBoxes = () => {
-  const randomColor = getRandomHexColor();
-  const boxEl = document.createElement('div');
-  boxEl.style.width = `${size}px`; // Ustawienie szerokości
-  boxEl.style.height = `${size}px`; // Ustawienie wysokości
-  boxEl.style.backgroundColor = randomColor;
-
-  boxesContainer.appendChild(boxEl); // Dodanie wygenerowanego kwadratu do kontenera
-  
-  size += 10; // Zwiększenie rozmiaru dla kolejnego kwadratu
-};
-
-createEl.addEventListener('click', createBoxes);
+const input = document.querySelector('.buttons-input')
+console.log(input)
